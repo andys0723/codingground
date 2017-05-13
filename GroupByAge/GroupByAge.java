24b874c0887fb5449be142596c202608cc778459
andys0723 <andys0723@gmail.com>
2017-05-13 13:29:51 -0700

    Update GroupByAge.java

--- GroupByAge/GroupByAge.java
+++ GroupByAge/GroupByAge.java
@@ -15,7 +15,18 @@ public void groupByAge(List<Person> people){
         offset += kc.getValue();
     }
     
-    while(){
+    while(!ageToOffset.isEmpty()){
+        Map.Entry<Integer, Integer> from = ageToOffset.iterator().next();
+        Integer toAge   = people.get(from.getValue()).age;
+        Integer toValue = ageToOffset.get(toAge);
+        Collections.swap(people, from.getValue(), toValue);
+        Integer count = ageToCount.get(toAge) - 1;
+        agetToCount.put(toAge, count);
+        if(count > 0){
+            ageToOffset.put(toAge, tovalue + 1);
+        }else{
+            ageToOffset.remove(toAge);
+        }
         
     }
     
